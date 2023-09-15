d9c909982ae3e6c5bdb327c27ddf8da5f9eb337f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2023-09-15 14:25:59 +0300

    Trees Representation and Traversal (BFS, DFS)
    Lecture Trees Representation and Traversal (BFS, DFS)

---
+++ TreesRepresentationAndTraversal(BFS,DFS)/main/java/implementations/Tree.java
@@ -0,0 +1,221 @@
+package implementations;
+
+import interfaces.AbstractTree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Tree<E> implements AbstractTree<E> {
+
+    private E value;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+
+    public Tree(E value, Tree<E>... subtrees) {
+        this.value = value;
+        this.parent = null;
+        this.children = new ArrayList<>();
+
+        for (Tree<E> subtree : subtrees) {
+            this.children.add(subtree);
+            subtree.parent = this;
+
+        }
+    }
+
+    @Override
+    public List<E> orderBfs() {
+
+        List<E> result = new ArrayList<>();
+        if(this.value == null) {
+            return result;
+        }
+        Deque<Tree<E>> childrenQueue = new ArrayDeque<>();
+
+        childrenQueue.offer(this);
+
+        while (!childrenQueue.isEmpty()){
+        Tree<E> current = childrenQueue.poll();
+
+            result.add(current.value);
+
+            for (Tree<E> child : current.children) {
+
+                childrenQueue.offer(child);
+
+            }
+        }
+
+
+        return result;
+    }
+
+    @Override
+    public List<E> orderDfs() {
+
+        List<E> result = new ArrayList<>();
+
+
+
+         this.doDFS(this, result);
+
+//          за orderDfs със Stack
+//        Deque<Tree<E>> toTravers = new ArrayDeque<>();
+//
+//        toTravers.push(this);
+//
+//        while (!toTravers.isEmpty()){
+//            Tree<E> current = toTravers.pop();
+//
+//            for (Tree<E> node : current.children) {
+//                toTravers.push(node);
+//            }
+//            result.add(current.value);
+//        }
+
+        return result;
+    }
+
+    private void doDFS(Tree<E> node, List<E> result) {
+
+        for (Tree<E> child : node.children) {
+            this.doDFS(child, result);
+        }
+
+        result.add(node.value);
+    }
+
+    @Override
+    public void addChild(E parentKey, Tree<E> child) {
+        // за find с DFS
+      //  Tree<E> search = find(this, parentKey);
+
+        // за find с BFS
+        Tree<E> search = find(parentKey);
+
+        if(search == null){
+            throw new IllegalArgumentException();
+        }
+
+        search.children.add(child);
+        child.parent = search;
+
+    }
+//             find с BFS
+    private Tree<E> find(E parentKey){
+
+
+        Deque<Tree<E>> childrenQueue = new ArrayDeque<>();
+
+        childrenQueue.offer(this);
+
+        while (!childrenQueue.isEmpty()){
+            Tree<E> current = childrenQueue.poll();
+
+            if (current.value.equals(parentKey)) {
+                return current;
+            }
+
+            for (Tree<E> child : current.children) {
+
+                childrenQueue.offer(child);
+
+            }
+        }
+
+        return null;
+
+
+//       fins с DFS
+//    private Tree<E> find(Tree<E> current, E parentKey){
+//
+//        if(current.value.equals(parentKey)) {
+//            return current;
+//        }
+//        for (Tree<E> child : current.children) {
+//            Tree<E> found = this.find(child, parentKey);
+//            if (found != null) {
+//                return found;
+//            }
+//        }
+//
+//
+//        return null;
+
+
+    }
+
+    @Override
+    public void removeNode(E nodeKey) {
+
+        Tree<E> toRemove = find(nodeKey);
+
+        if(toRemove == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (Tree<E> child : toRemove.children){
+            child.parent = null;
+        }
+            toRemove.children.clear();
+
+        Tree<E> parent = toRemove.parent;
+
+        if(parent != null) {
+            parent.children.remove(toRemove);
+        }
+
+        toRemove.value = null;
+    }
+
+    @Override
+    public void swap(E firstKey, E secondKey) {
+
+        Tree<E> firstNode = find(firstKey);
+        Tree<E> secondNode = find(secondKey);
+
+        if(firstNode == null || secondNode == null){
+            throw new IllegalArgumentException();
+        }
+
+        Tree<E> firstParent = firstNode.parent;
+        Tree<E> secondParent = secondNode.parent;
+
+        if(firstParent == null) {
+            swapRoot(secondNode);
+            return;
+        } else if (secondParent == null) {
+            swapRoot(firstNode);
+            return;
+        }
+
+
+        firstNode.parent = secondParent;
+        secondNode.parent = firstParent;
+
+
+        int firstIndex = firstParent.children.indexOf(firstNode);
+        int secondIndex = secondParent.children.indexOf(secondNode);
+
+
+
+        firstParent.children.set(firstIndex, secondNode);
+        secondParent.children.set(secondIndex, firstNode);
+
+
+
+    }
+
+    private void swapRoot(Tree<E> node) {
+        this.value = node.value;
+        this.parent = null;
+        this.children = node.children;
+        node.parent = null;
+    }
+}
+
+
+
